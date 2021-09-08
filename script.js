697efd2098f489f2d6e38a02a32328614cb4a58a
Hackr <81916900+123452123-codr@users.noreply.github.com>
2021-09-08 11:46:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-analytics.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCX_J_3OFvZd0Yk4zkWR_aaIaf5UMLWZGY",
     authDomain: "trial-chat-pro.firebaseapp.com",
